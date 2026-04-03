b535e80000e4127b877e7978e7685e0ebc45c64c
Vaynnore <wiktor5wiktor@wp.pl>
2026-04-03 16:57:18 +0200

    Enhance ripuntu.sh with user prompts and checks
    Added user prompts and installation instructions to the script.

--- ripuntu.sh
+++ ripuntu.sh
@@ -1,8 +1,32 @@
-sudo apt install lxterminal
-apt update
-apt install software-properties-common
-sudo add-apt-repository universe
-sudo add-apt-repository multiverse
-sudo apt update
-sudo apt install nano dpkg dbus-x11 tasksel openbox tint2 htop firefox obmenu nitrogen gedit tigervnc-standalone-server tigervnc-common rofi ubuntu-wallpapers lightdm obmenu gnome-software tunar gvfs gvfs-afp gvfs-smb -y
+#!/bin/bash
+
+## this is ripuntu installer.
+## do chmod +x ripuntu.sh and run ./ripuntu.sh without sudo to prevent install issues.
+echo
+echo "Install will start soon please be shure that you are on sudo user other than root"
+echo
+sleep 1
+echo
+echo "You will be asked for password and consent to install programs"
+echo
+sleep 1
+echo "Continue? (y/n)"
+read -n 1 answer
+echo "" 
+
+if [ "$answer" = "y" ]; then
+    echo "Installing"
+    sudo apt update && sudo apt full-upgrade -y
+    sudo apt install lxterminal -y
+    sudo apt install software-properties-common -y
+    sudo add-apt-repository -y universe
+    sudo add-apt-repository -y multiverse
+    sudo apt update -y
+    sudo apt install nano dpkg dbus-x11 tasksel openbox tint2 htop firefox obmenu nitrogen gedit tigervnc-standalone-server tigervnc-common rofi ubuntu-wallpapers lightdm obmenu gnome-software nautilus gvfs gvfs-afp gvfs-smb -y
+else
+    echo "Canceled."
+fi
+
+
+
 
