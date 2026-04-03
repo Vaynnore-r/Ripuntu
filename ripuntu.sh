fc98e318b21f3879b7055fbfbefdb2c077b0a83b
Vaynnore <wiktor5wiktor@wp.pl>
2026-04-03 14:07:37 +0200

    Add files via upload

---
+++ ripuntu.sh
@@ -0,0 +1,8 @@
+sudo apt install lxterminal
+apt update
+apt install software-properties-common
+sudo add-apt-repository universe
+sudo add-apt-repository multiverse
+sudo apt update
+sudo apt install nano dpkg dbus-x11 tasksel openbox tint2 htop firefox obmenu nitrogen gedit tigervnc-standalone-server tigervnc-common rofi ubuntu-wallpapers lightdm obmenu gnome-software tunar gvfs gvfs-afp gvfs-smb -y
+
