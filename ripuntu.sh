0961d41681a80c1fe4fd00ea3464e2b8d4409cd3
Vaynnore <268145842+Vaynnore-r@users.noreply.github.com>
2026-05-03 09:53:57 +0200

    Update ripuntu.sh

--- ripuntu.sh
+++ ripuntu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-## this is ripuntu installer.
-## do chmod +x ripuntu.sh and run ./ripuntu.sh without sudo to prevent install issues.
+## This is ripuntu installer.
+## Do chmod +x ripuntu.sh and run ./ripuntu.sh without sudo to prevent install issues.
 echo
 echo "Install will start soon please be shure that you are on sudo user other than root"
 echo
