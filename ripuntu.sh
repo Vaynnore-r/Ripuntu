0e11203adc8d97743116aa096ea56273941e45e0
Vaynnore <268145842+Vaynnore-r@users.noreply.github.com>
2026-05-03 09:54:01 +0200

    added openbox readme and added starship install to ripuntu.sh

--- ripuntu.sh
+++ ripuntu.sh
@@ -17,12 +17,13 @@ echo ""
 if [ "$answer" = "y" ]; then
     echo "Installing"
     sudo apt update && sudo apt full-upgrade -y
-    sudo apt install lxterminal -y
+    sudo apt install lxterminal curl -y
     sudo apt install software-properties-common -y
     sudo add-apt-repository -y universe
     sudo add-apt-repository -y multiverse
     sudo apt update -y
     sudo apt install nano dpkg dbus-x11 tasksel openbox openbox-menu tint2 htop firefox obmenu nitrogen gedit tigervnc-standalone-server tigervnc-common rofi ubuntu-wallpapers lightdm gnome-software nautilus gvfs gvfs-afp gvfs-smb -y
+    curl -sS https://starship.rs | sh -s -- -y
     sudo systemctl enable lightdm 
     echo "Installation compleate, system will reboot in 3 seconds"
     sleep 3
